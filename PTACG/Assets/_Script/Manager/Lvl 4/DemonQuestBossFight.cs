0fcdbf99768c6b999554a02df3d8b382003c4c7e
Nsk0033 <nsk0033@gmail.com>
2024-03-19 14:47:06 +0800

    update boss health, added game over

--- PTACG/Assets/_Script/Manager/Lvl 4/DemonQuestBossFight.cs
+++ PTACG/Assets/_Script/Manager/Lvl 4/DemonQuestBossFight.cs
@@ -5,15 +5,33 @@ using UnityEngine;
 public class DemonQuestBossFight : MonoBehaviour
 {
     private bool hasTriggered = false;
+	private bool gateOpened = false;
 	[SerializeField] private GameObject boss;
 	[SerializeField] private GameObject canva;
 	[SerializeField] private GameObject wall;
+	[SerializeField] private GameObject wall1;
+	
+	
 	
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!hasTriggered && other.CompareTag("Player"))
         {
-            Lvl4UIManager uiManager = GameObject.FindObjectOfType<Lvl4UIManager>();
+			wall.SetActive(true);
+			wall1.SetActive(true);
+			
+            ResetBossHealth resetBossHealth = boss.GetComponent<ResetBossHealth>();
+			if (resetBossHealth != null)
+			{
+				resetBossHealth.ResetHealth();
+				Debug.Log("ResetHealth");
+			}
+			else
+			{
+				Debug.LogWarning("ResetBossHealth component not found.");
+			}
+			
+			Lvl4UIManager uiManager = GameObject.FindObjectOfType<Lvl4UIManager>();
             if (uiManager != null)
             {
                 uiManager.StartCoroutine(uiManager.DemonQuestBossFight());
@@ -22,28 +40,44 @@ public class DemonQuestBossFight : MonoBehaviour
             {
                 Debug.LogWarning("Lvl4UIManager component not found in the scene.");
             }
-            hasTriggered = true; // Mark the trigger as activated
         }
+		
+		
     }
+	
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			if (canva != null)
+			{
+				canva.SetActive(false);
+			}
+		}
+	}
 	
 	private void Update()
     {
         // Check if the GameObject to monitor has been destroyed
-        if (boss == null)
+        if (boss == null && !gateOpened)
         {
-			canva.SetActive(false);
-			wall.SetActive(false);
-            // GameObject has been destroyed
-            Debug.Log("GameObject has been destroyed!");
+            OpenGate();
+            gateOpened = true; // Set the flag to true after opening the gate
         }
-		if (!boss.activeSelf)
+        if (!boss.activeSelf && !gateOpened)
         {
-			canva.SetActive(false);
-			wall.SetActive(false);
-            // GameObject has been destroyed
-            Debug.Log("GameObject has been destroyed!");
+            OpenGate();
+            gateOpened = true; // Set the flag to true after opening the gate
         }
     }
 	
-	
+	private void OpenGate()
+	{
+		canva.SetActive(false);
+		wall.SetActive(false);
+		wall1.SetActive(false);
+		hasTriggered = true; // Mark the trigger as activated
+		// GameObject has been destroyed
+		Debug.Log("GameObject has been destroyed!");
+	}
 }
--- PTACG/Assets/_Scripts/Health/Health.cs
+++ PTACG/Assets/_Scripts/Health/Health.cs
@@ -9,7 +9,7 @@ public class Health : MonoBehaviour
  
     [Header("Health")]
     [SerializeField] private float initialHealth = 10f;
-    [SerializeField] private float maxHealth = 10f;
+    public float maxHealth = 10f;
 
     [Header("Shield")] 
     [SerializeField] private float initialShield = 5f;
@@ -19,6 +19,7 @@ public class Health : MonoBehaviour
     [SerializeField] private bool destroyObject;
 	[SerializeField] private float damageCooldown = 0.75f; // Cooldown period after taking damage
 	[SerializeField] private bool canTakeDamage = true; // Flag to control if the player can take damage
+	[SerializeField] private float timeToDestroy = 2f;
 
     private Character character;
     private CharacterController controller;
@@ -26,6 +27,7 @@ public class Health : MonoBehaviour
     private SpriteRenderer spriteRenderer;
     private EnemyHealth enemyHealth;
     private BossBaseShot bossBaseShot;
+	
 
     private bool isPlayer;
     private bool shieldBroken;
@@ -51,7 +53,6 @@ public class Health : MonoBehaviour
 
         CurrentHealth = initialHealth;
         CurrentShield = initialShield;
-        isShieldBroken = shieldBroken;
 
         if (character != null)
         {
@@ -65,9 +66,8 @@ public class Health : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.L))
         {
-            TakeDamage(1);
+            TakeDamage(5);
         }
-        Debug.Log("health script: " + shieldBroken);
     }
 
     // Take the amount of damage we pass in parameters
@@ -129,7 +129,8 @@ public class Health : MonoBehaviour
 
         if (bossBaseShot != null)
         {
-            OnBossDead?.Invoke();
+			Invoke("DisableAfterTime",timeToDestroy);
+			
         }
 
         if (destroyObject)
@@ -137,6 +138,11 @@ public class Health : MonoBehaviour
             DestroyObject();
         }
     }
+	
+	private void DisableAfterTime()
+	{
+		gameObject.SetActive(false);
+	}
     
     // Revive this game object    
     public void Revive()
@@ -189,13 +195,28 @@ public class Health : MonoBehaviour
         // Update Boss health
         if (bossBaseShot != null && character.CharacterType == Character.CharacterTypes.AI)
         {
-            UIManager.Instance.UpdateBossHealth(CurrentHealth, maxHealth);
-        }  
+			if(Lvl4UIManager.Instance != null)
+			{
+				Lvl4UIManager.Instance.UpdateBossHealth(gameObject.tag, CurrentHealth, maxHealth);
+			}
+         
+			else if(UIManager.Instance != null)
+			{
+				UIManager.Instance.UpdateBossHealth(CurrentHealth, maxHealth);
+			}
+        }
       
         // Update Player health
         if (character != null && bossBaseShot == null && character.CharacterType == Character.CharacterTypes.Player)
         {
-            UIManager.Instance.UpdateHealth(CurrentHealth, maxHealth, CurrentShield, maxShield, isPlayer);
+			if(Lvl4UIManager.Instance != null)
+			{
+				Lvl4UIManager.Instance.UpdateHealth(CurrentHealth, maxHealth, CurrentShield, maxShield, isPlayer);
+			}
+			else if(UIManager.Instance != null)
+			{
+				UIManager.Instance.UpdateHealth(CurrentHealth, maxHealth, CurrentShield, maxShield, isPlayer);
+			}
         }
     }   
 }
